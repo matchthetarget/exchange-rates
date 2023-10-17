07b861c1d3fd1d57b9ae04972d6432791d96479d
Ben Purinton <ben.purinton@gmail.com>
2023-10-17 12:37:20 -0400

    added key for exchange rates

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,10 +1,12 @@
 class CurrenciesController < ApplicationController
 
   def list_pairs
-    api_url = "https://api.exchangerate.host/symbols"
+    # api_url = "https://api.exchangerate.host/symbols"
+    api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATES_KEY"]}"
     raw_data = URI.open(api_url).read
     parsed_data = JSON.parse(raw_data)
-    @symbols = parsed_data.fetch("symbols").keys
+    # @symbols = parsed_data.fetch("symbols").keys
+    @symbols = parsed_data.fetch("currencies").keys
 
     render("currency_templates/index")
   end
@@ -12,10 +14,12 @@ class CurrenciesController < ApplicationController
   def first_currency
     @original_currency = params.fetch("from_currency")
 
-    api_url = "https://api.exchangerate.host/symbols"
+    # api_url = "https://api.exchangerate.host/symbols"
+    api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATES_KEY"]}"
     raw_data = URI.open(api_url).read
     parsed_data = JSON.parse(raw_data)
-    @symbols = parsed_data.fetch("symbols").keys
+    # @symbols = parsed_data.fetch("symbols").keys
+    @symbols = parsed_data.fetch("currencies").keys
 
     render("currency_templates/step_one")
   end
@@ -24,11 +28,13 @@ class CurrenciesController < ApplicationController
     @original_currency = params.fetch("from_currency")
     @destination_currency = params.fetch("to_currency")
 
-    api_url = "https://api.exchangerate.host/convert?from=#{@original_currency}&to=#{@destination_currency}"
+    # api_url = "https://api.exchangerate.host/convert?from=#{@original_currency}&to=#{@destination_currency}"
+    api_url = "http://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATES_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
     raw_data = URI.open(api_url).read
     parsed_data = JSON.parse(raw_data)
     
-    @exchange_rate = parsed_data.fetch("info").fetch("rate")
+    # @exchange_rate = parsed_data.fetch("info").fetch("rate")
+    @exchange_rate = parsed_data.fetch("result")
     
     render("currency_templates/step_two")
   end
